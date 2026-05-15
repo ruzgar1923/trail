61479bd2aed7b58df47949a6f11b2f2e15cb2829
ruzgar1923 <2010ruzgarvatansever2010@gmail.com>
2026-05-15 21:54:23 +0300

    Update ruzgar_ai_asistan.py

--- ruzgar_ai_asistan.py
+++ ruzgar_ai_asistan.py
@@ -6,7 +6,7 @@ import numpy as np
 # --- 1. AYARLAR VE API ---
 # Secrets'tan çekmeyi dener
 try:
-    GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+    GROQ_API_KEY = st.secrets["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"]
 except:
     GROQ_API_KEY = "" # Eğer hata alırsan buraya gsk_ ile başlayan kodu tırnak içine yazabilirsin
 
